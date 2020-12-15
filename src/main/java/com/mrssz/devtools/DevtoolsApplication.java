f3a6cd76b53ca95ff642e804e631d46e34b7e860
mrssz <1069889425@qq.com>
2020-12-16 01:43:33 +0800

    feat(Root):zeebe

--- src/main/java/com/mrssz/devtools/DevtoolsApplication.java
+++ src/main/java/com/mrssz/devtools/DevtoolsApplication.java
@@ -1,11 +1,15 @@
 package com.mrssz.devtools;
 
+import io.zeebe.spring.client.EnableZeebeClient;
+import io.zeebe.spring.client.annotation.ZeebeDeployment;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableZeebeClient
 @MapperScan("com.mrssz.devtools.dao")
+@ZeebeDeployment(classPathResources = "bpmn/devtools.bpmn")
 public class DevtoolsApplication {
 
     public static void main(String[] args) {
